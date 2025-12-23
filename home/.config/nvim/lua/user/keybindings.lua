d52d94888254b8b8ffa27f8a9d783e69f656b85f
Jakub Skowron <jakubskowron676@gmail.com>
2025-12-23 14:42:01 +0100

    chore: remove TODO markers

--- home/.config/nvim/lua/user/keybindings.lua
+++ home/.config/nvim/lua/user/keybindings.lua
@@ -1,33 +1,38 @@
 ---------------------
---FIXME: NORMAL MODE |
+-- NORMAL MODE       |
 ---------------------
 
---WARNING: Core
+-- Core
+--------
 
 vim.keymap.set("n", "<ESC>", "<CMD>noh<CR>", { desc = "Clear search highlights" })
 
---WARNING: Extensions
+-- Extensions
+--------------
 
---NOTE: trouble.nvim
+-- trouble.nvim
 vim.keymap.set("n", "<LEADER>td", "<CMD>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Buffer diagnostics" })
 vim.keymap.set("n", "<LEADER>ts", "<CMD>Trouble symbols toggle focus=false win.relative=win<CR>", { desc = "Symbols" })
 vim.keymap.set("n", "<LEADER>tl", "<CMD>Trouble lsp toggle focus=false win.position=right<CR>", { desc = "LSP definitions" })
 vim.keymap.set("n", "<LEADER>tL", "<CMD>Trouble loclist toggle<CR>", { desc = "Location list" })
 vim.keymap.set("n", "<LEADER>tq", "<CMD>Trouble qflist toggle<CR>", { desc = "Quickfix list" })
 
---NOTE: todo-comments.nvim
+-- todo-comments.nvim
 vim.keymap.set("n", "<LEADER>tt", "<CMD>TodoTrouble toggle<CR>", { desc = "Open TODOs" })
 
---NOTE: Oil.nvim
+-- Oil.nvim
 vim.keymap.set("n", "<LEADER>fb", "<CMD>Oil --float --preview %:p:h<CR>", { desc = "Open file browser" })
 
---NOTE: Buffers
+-- Buffers
 vim.keymap.set("n", "<LEADER>bf", "<CMD>lua MiniPick.builtin.buffers()<CR>", { desc = "Open buffer explorer" })
 vim.keymap.set("n", "<TAB>", "<CMD>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-TAB>", "<CMD>bprev<CR>", { desc = "Previous buffer" })
 
 ----------------------------
---FIXME: NORMAL/VISUAL MODE |
+-- NORMAL/VISUAL MODE       |
 ----------------------------
+
+-- Core
+--------
 
 vim.keymap.set({ "x", "n" }, "<LEADER>p", "\"_dP", { desc = "Paste without overwriting the paste register" })
